893c7e01c62e3492ef9aebbd1a1385307926e376
Marc MOREAU <contact@ancelade.com>
2023-08-22 00:32:55 +0200

    fic condition file not exist

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -e "/etc/install.lock" ]; then #Execute on firt start
+if [ ! -e "/etc/install.lock" ]; then #Execute on firt start
     wget -qO- https://raw.githubusercontent.com/Ancelade/VpsStarterScript/main/onInstall.sh | bash
     touch /etc/install.lock
 fi
