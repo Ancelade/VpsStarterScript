9a289fb08e57a4e16eeb1e7fa4cdf62c4a8837b0
PHMarc <moreau.marc54@gmail.com>
2023-08-21 23:50:30 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+
+
+if [ -e "/etc/install.lock" ]; then #Execute on firt start
+    wget -qO- https://raw.githubusercontent.com/Ancelade/VpsStarterScript/main/onInstall.sh | bash
+    touch /etc/install.lock
+fi
+
+#Execute on all start
+wget -qO- https://raw.githubusercontent.com/Ancelade/VpsStarterScript/main/onStart.sh | bash
 
